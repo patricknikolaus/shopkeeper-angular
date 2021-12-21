575fab9aa3742cc7fcf454882d629f6711abb1dd
Patrick <pdn6991@gmail.com>
2021-12-21 11:50:15 -0500

    adds nav-bar component

---
+++ src/app/components/game-index/game-index/game-index.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-index',
+  templateUrl: './game-index.component.html',
+  styleUrls: ['./game-index.component.scss']
+})
+export class GameIndexComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
